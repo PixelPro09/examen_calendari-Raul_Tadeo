4b0c3005d03587597ea8346243c3a32755087763
PixelPro09 <tadeojones9@gmail.com>
2022-11-15 19:20:15 +0100

    crear esdeveniment y meterlo en la lista pero no va aparentemente da error, con ayuda de barahona

--- lib/controlador.dart
+++ lib/controlador.dart
@@ -1,7 +1,23 @@
 import 'package:examen_calendari/model.dart';
 
 class Controlador{
+  DateTime? horaInici, horaFinal;
+  String? titol;
+  String? descripcio;
+
   List <Esdeveniment> getListaEsdeveniments(){
     return Modelo().esdeveniments;
+  }
+
+  void save() {
+    Modelo().afegirEsdeveniments(
+        Esdeveniment(
+        horaInici: horaInici!,
+        horaFinal: horaFinal!,
+        titol: titol!,
+        descripcio: descripcio
+        )
+    );
+
   }
 }
--- lib/editar_esdeveniment.dart
+++ lib/editar_esdeveniment.dart
@@ -1,56 +1,87 @@
 import 'package:flutter/material.dart';
 
+import 'controlador.dart';
 
 class EditarEsdevenimentForm extends StatefulWidget {
-  const EditarEsdevenimentForm({Key? key}) : super(key: key);
-
+  EditarEsdevenimentForm({Key? key}) : super(key: key);
+  final Controlador elControlador = Controlador();
   @override
   State<EditarEsdevenimentForm> createState() => _EditarEsdevenimentFormState();
 }
 
 class _EditarEsdevenimentFormState extends State<EditarEsdevenimentForm> {
+  final _clauFormulari = GlobalKey<FormState>();
 
-  final _clauFormulari=GlobalKey<FormState>();
+
+  String? validator(String? valor) {
+    if (valor != null) {
+      return null;
+    } else {
+      return "Test";
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Calendari Raul Tadeo"),),
-      body: Column(
-        children: [
-          Row(children: [
-            Expanded(
-              child: TextFormField(
-                decoration: const InputDecoration(
-                  labelText: "Data inici"
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.save),
+        onPressed: (){
+          if(_clauFormulari.currentState?.validate() == true){
+            _clauFormulari.currentState?.save();
+            widget.elControlador.save();
+          }
+          Navigator.of(context).pop();
+        },
+      ),
+      appBar: AppBar(
+        title: const Text("Calendari Raul Tadeo"),
+      ),
+      body: Form(
+        key: _clauFormulari,
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  child: TextFormField(
+                    decoration: const InputDecoration(labelText: "Data inici"),
+                    validator: validator,
+                    onSaved: (valor){
+                      widget.elControlador.horaInici = DateTime.parse(valor!);
+                    },
+                  ),
                 ),
-              ),
+                Expanded(
+                  child: TextFormField(
+                    decoration: const InputDecoration(labelText: "Data fi"),
+                    validator: validator,
+                    onSaved: (valor){
+                      widget.elControlador.horaInici = DateTime.parse(valor!);
+                    },
+                  ),
+                )
+              ],
+            ),
+            TextFormField(
+              decoration: const InputDecoration(labelText: "Títol"),
+              validator: validator,
+              onSaved: (valor){
+                widget.elControlador.horaInici = DateTime.parse(valor!);
+              },
             ),
             Expanded(
               child: TextFormField(
+                maxLines: null,
+                expands: true,
                 decoration: const InputDecoration(
-                    labelText: "Data fi"
+                  labelText: "Descripció",
+                  alignLabelWithHint: true,
                 ),
               ),
             )
           ],
-          ),
-          TextFormField(
-            decoration: const InputDecoration(
-              labelText: "Títol"
-            ),
-          ),
-          Expanded(
-            child: TextFormField(
-              maxLines: null,
-              expands: true,
-              decoration: const InputDecoration(
-                labelText: "Descripció",
-                alignLabelWithHint: true,
-              ),
-            ),
-          )
-        ],
+        ),
       ),
     );
   }
